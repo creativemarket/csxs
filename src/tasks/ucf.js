9fb2587a83ad77c3ba947c120040bdff877dc830
Brian Reavis <brian@thirdroute.com>
2013-04-26 20:59:56 -0700

    Workaround for UCF tool errors caused by paths with spaces (#2).

--- src/tasks/ucf.js
+++ src/tasks/ucf.js
@@ -13,13 +13,20 @@
  * @author Brian Reavis <brian@creativemarket.com>
  */
 
+var fs    = require('fs');
+var os    = require('os');
 var spawn = require('child_process').spawn;
-var path = require('path');
+var path  = require('path');
 
 
 roto.defineTask('csxs.ucf', function(callback, options) {
-	var err = false;
-	var args = [
+	var ucf, args, err;
+	var path_tmp     = (os.tmpdir || os.tmpDir)();
+	var path_project = process.cwd();
+	var path_output  = path.normalize(path_tmp + '/' + path.basename(options.output));
+	var path_input   = options.input;
+
+	args = [
 		'-jar',
 		path.resolve(__dirname, '../../bin/ucf.jar'),
 		'-package',
@@ -27,14 +34,14 @@ roto.defineTask('csxs.ucf', function(callback, options) {
 		'-keystore', options.keystore,
 		'-storepass', options.password,
 		'-tsa', 'https://timestamp.geotrust.com/tsa',
-		options.output,
-		'-C', options.input, '.'
+		path_output,
+		'-C', path_input, '.'
 	];
 
 	console.log(roto.colorize(options.output, 'white'));
 	console.log(roto.colorize('java ' + args.join(' '), 'magenta'));
 
-	var ucf = spawn('java', args);
+	ucf = spawn('java', args);
 	ucf.stdout.on('data', function (data) { process.stdout.write(data); });
 	ucf.stderr.on('data', function (data) { process.stderr.write(data); err = true; });
 	ucf.on('exit', function(code) {
@@ -42,6 +49,7 @@ roto.defineTask('csxs.ucf', function(callback, options) {
 			console.error(roto.colorize('ERROR: ', 'red') + 'Unable to create ZXP package.');
 			callback(false);
 		} else {
+			fs.renameSync(path_output, path.normalize(path_project + '/' + options.output));
 			callback();
 		}
 	});
